d9718c4fb3ce521f83346c08d680ff261df96f5a
rnfelice <ryanfelice@gmail.com>
2020-05-25 17:09:41 +0100

    new version of check.curves that can detect cryptic missing curves

--- R/check.curves.R
+++ R/check.curves.R
@@ -6,33 +6,24 @@
 
 
 check_curves <- function(lm.tibble,ncurves){
-  uniques <- lm.tibble %>% group_by(spec.id) %>% filter(class == "C") %>% summarise(Unique_Elements = n_distinct(id))
-  #max_curves <- max(uniques$Unique_Elements)
-  too_few <- which(uniques$Unique_Elements < ncurves)
-  if (length(too_few)==0){
+  uniques <- lm.tibble %>% group_by(spec.id) %>% filter(class == "C")
+  missinglist<-c()
+  extraslist<-c()
+  for (i in 1:length(unique(lm.tibble$spec.id))){
+  x <- uniques %>% filter(spec.id == filenames[i]) %>% pull(id)
+  missings1 <- setdiff(as.character(c(1:ncurves)),x)
+  extras1 <- setdiff(x, as.character(c(1:ncurves)))
+  if(length(missings1!=0)){
+    writeLines(paste("WARNING!!", filenames[i], "is missing curve number", missings1,"\n"))
+    missinglist<-c(missinglist,filenames[i])}
+  if(length(extras1!=0)){
+    writeLines(paste("WARNING!!",filenames[i], "has an extra curve number", extras1,"\n"))
+    extraslist<-c(extraslist,filenames[i])}
+  }
+  if(length(missinglist)==0){
     writeLines("You have no missing curves! Great job!")
   }
-  else{
-    for (i in 1:length(too_few)){
-      name <- uniques$spec.id[too_few[i]]
-      present <- lm.tibble %>% group_by(spec.id) %>% filter(class == "C") %>% filter(spec.id == name) %>% distinct(id) %>% pull(id) %>% as.vector()
-      absent <- setdiff(as.character(1:ncurves), present)
-      writeLines("WARNING!!!!!")
-      writeLines(paste(name, "is missing curve number", absent))
-    }
-  }
-  too_many <- which(uniques$Unique_Elements > ncurves)
-  if (length(too_many)==0){
+  if(length(extraslist)==0){
     writeLines("You have no extra curves! Great job!")
   }
-  else{
-    for (i in 1:length(too_many)){
-      name <- uniques$spec.id[too_many[i]]
-      present <- lm.tibble %>% group_by(spec.id) %>% filter(class == "C") %>% filter(spec.id == name) %>% distinct(id) %>% pull(id) %>% as.vector()
-      absent <- setdiff(present,as.character(1:ncurves))
-      writeLines("WARNING!!!!!")
-      writeLines(paste(name, "has extra curve number", absent))
-    }
   }
-
-}
